7df133512559855d191277a0192a26c255f1ebb1
Mathuto04 <102726905+Mathuto04@users.noreply.github.com>
2022-08-09 12:28:13 +0200

    Add project files.

---
+++ SignIn/SignIn/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+namespace SignIn
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new SignIn.Pages.LoginPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts  
+        }
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps  
+        }
+        protected override void OnResume()
+        {
+            // Handle when your app resumes  
+        }
+    }
+}
---
+++ SignIn/SignIn/Pages/LoginPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using SignIn.ViewModels;
+using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SignIn.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            var vm = new LoginViewModel();
+            this.BindingContext = vm;
+            vm.DisplayInvalidLoginPrompt += () => DisplayAlert("Error", "Invalid Login, try again", "OK");
+            InitializeComponent();
+
+            Email.Completed += (object sender, EventArgs e) =>
+            {
+                Password.Focus();
+            };
+
+            Password.Completed += (object sender, EventArgs e) =>
+            {
+                vm.SubmitCommand.Execute(null);
+            };
+        }
+               
+    }
+}
